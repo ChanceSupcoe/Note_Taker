ca9817e6576902a064158c6e66d1f1ca5191d602
Chance Supcoe <chancesupcoe@gmail.com>
2022-04-07 15:51:27 -0400

    server.js added

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, () =>
+  console.log(`App listening at:${PORT} 🚀`)
+);
